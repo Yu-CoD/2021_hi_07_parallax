a55c8d95e7986366f27f5f8fb016bb07e12db44f
Yu-CoD <dbdnjs09@gmail.com>
2021-03-29 12:21:18 +0900

    header-wrapper active 구현

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(window).scroll(function(){
+	var scTop = $(this).scrollTop()
+/* 	console.log(scTop) */
+	if(scTop > 200){
+		if(!$('.header-wrapper').hasClass('active')) {
+			$('.header-wrapper').css('top', '-60px')
+			$('.header-wrapper').stop().animate({"top":0}, 300)
+			$('.header-wrapper').addClass('active')
+		}
+	}
+	else {
+		$('.header-wrapper').removeClass('active')
+	}
+})
