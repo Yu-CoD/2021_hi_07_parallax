cc5d0729ccc2aabdb72c0d2a44c6cf601bc65539
Yu-CoD <dbdnjs09@gmail.com>
2021-03-30 16:54:10 +0900

    parallax mb.ver 수정

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,31 @@
-$(window).scroll(function(){
+/* $(window).scroll(function() {
 	var scTop = $(this).scrollTop()
-/* 	console.log(scTop) */
-	if(scTop > 200){
+	if(scTop > 200) {
 		if(!$('.header-wrapper').hasClass('active')) {
 			$('.header-wrapper').css('top', '-60px')
-			$('.header-wrapper').stop().animate({"top":0}, 300)
+			$('.header-wrapper').stop().animate({'top': 0}, 300)
 			$('.header-wrapper').addClass('active')
 		}
 	}
 	else {
+		$('.header-wrapper').removeClass('active')
+	}
+}) */
+
+
+
+$(window).scroll(function(){
+	var scTop = $(this).scrollTop()
+/* 	console.log(scTop) */
+	if(scTop > 200){
+		$('.header-wrapper').css('transform', 'translateY(-100%)')
+		$('.header-wrapper').css('transform')
+		$('.header-wrapper').addClass('active')
+		$('.header-wrapper').css('transform', 'translateY(0)')
+		}
+	}
+	else {
+		$('.header-wrapper').css('transform', 'translateY(0)')
 		$('.header-wrapper').removeClass('active')
 	}
 })
